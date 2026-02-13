4da448521888a63a1bd93360df57f2de31c81984
Arthur Parente <arthurparente26@gmail.com>
2026-02-12 22:14:01 -0300

    :ok_hand: style: alter option in settings

--- resources/js/layouts/settings/Layout.vue
+++ resources/js/layouts/settings/Layout.vue
@@ -3,7 +3,6 @@ import Heading from '@/components/Heading.vue';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
 import { toUrl, urlIsActive } from '@/lib/utils';
-import { edit as editAppearance } from '@/routes/appearance';
 import { edit as editProfile } from '@/routes/profile';
 import { show } from '@/routes/two-factor';
 import { edit as editPassword } from '@/routes/user-password';
@@ -12,21 +11,13 @@ import { Link } from '@inertiajs/vue3';
 
 const sidebarNavItems: NavItem[] = [
     {
-        title: 'Profile',
+        title: 'Editar perfil',
         href: editProfile(),
     },
     {
-        title: 'Password',
+        title: 'Alterar senha',
         href: editPassword(),
-    },
-    {
-        title: 'Two-Factor Auth',
-        href: show(),
-    },
-    {
-        title: 'Appearance',
-        href: editAppearance(),
-    },
+    }
 ];
 
 const currentPath = typeof window !== undefined ? window.location.pathname : '';
