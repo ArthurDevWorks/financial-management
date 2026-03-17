f13c446c5a9f5c2a1a899afe51fa023ef5bf779f
Arthur Parente <arthurparente26@gmail.com>
2026-03-17 18:20:02 -0300

    :lipstick: feat: fix text home style

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -70,7 +70,7 @@ import {
                     </span>
                 </div>
                 
-                <h1 class="text-5xl sm:text-6xl lg:text-7xl font-bold mb-6 bg-gradient-to-r from-cyan-400 to-blue-400 bg-clip-text text-transparent">
+                <h1 class="text-4xl sm:text-5xl lg:text-6xl font-bold mb-6 bg-gradient-to-r from-cyan-400 to-blue-400 bg-clip-text text-transparent text-balance">
                     Controle Total de Suas Finanças
                 </h1>
                 
